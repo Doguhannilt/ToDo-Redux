9443ae9cb410c878a7066214df590158c74f8815
Doğuhan İlter <doguhannilt@gmail.com>
2024-01-12 15:31:37 +0300

    Todo backend with Redux is completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
+import Todo from './components/Todo.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <Todo/>
+    </Provider>
   </React.StrictMode>,
 )
---
+++ src/redux/store.js
@@ -0,0 +1,6 @@
+import {createStore} from 'redux'
+import todoReducer from './reducers'
+
+const store = createStore(todoReducer)
+
+export default store;
